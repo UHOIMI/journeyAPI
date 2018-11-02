565b79515195f0acc6b8fbf63bad141ca2c0a7ee
sanwa03 <g015c1124@gmail.com>
2018-11-02 10:03:05 +0900

    トークンエラー理由を表示するように変更

--- verifyToken.js
+++ verifyToken.js
@@ -2,28 +2,28 @@ var jwt = require('jsonwebtoken');
 var Auconfig = require('./Auconfig');
 function verifyToken(req, res, next) {
   // header か　url parameters か post parametersからトークンを取得する
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-  if (token) {
+    if (token) {
 
     // jwtの認証をする
-    jwt.verify(token, Auconfig.secret, function(error, decoded) {
-      if (error) {
-        return res.json({ status:'404',record: null, message: 'トークンの認証に失敗しました。' });
-      } else {
-        // 認証に成功したらdecodeされた情報をrequestに保存する
-        req.decoded = decoded;
-        console.log(decoded);
-        next();
-      }
-    });
+        jwt.verify(token, Auconfig.secret, function(error, decoded) {
+        if (error) {
+            return res.json({ status:'404',record: error.name, message: error.message });
+        } else {
+            // 認証に成功したらdecodeされた情報をrequestに保存する
+            req.decoded = decoded;
+            console.log(decoded);
+            next();
+        }
+        });
 
-  } else {
-    // トークンがなければエラーを返す
-    return res.status(403).send({
+    } else {
+        // トークンがなければエラーを返す
+        return res.status(403).send({
         status:'404',record: null, message: 'トークンがありません' 
     });
 
-  }
+    }
 }
 module.exports = verifyToken;
