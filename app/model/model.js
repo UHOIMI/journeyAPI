d2b46b0e68b1ecfa981fe1faabeb18165a617ae6
sanwa03 <g015c1124@gmail.com>
2018-12-05 12:06:06 +0900

    リレーションの追加

--- app/model/model.js
+++ app/model/model.js
@@ -8,5 +8,7 @@ model.favorite = require('./favoriteModel');
 model.users.hasOne(model.plan,{foreignKey: 'user_id'});
 model.plan.belongsTo(model.users,{foreignKey: 'user_id'});
 
+model.plan.hasMany(model.spot,{foreignKey: 'plan_id'});
+model.plan.belongsTo(model.spot,{foreignKey: 'plan_id'});
 
 module.exports = model;
